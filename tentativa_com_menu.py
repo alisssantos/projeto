05257438736744905580c253713b21a72bdb8915
alisssantos <j.alisson01@hotmail.com>
2022-05-24 18:35:42 -0300

    atualização 24/05/2022

--- tentativa_com_menu.py
+++ tentativa_com_menu.py
@@ -2,10 +2,10 @@ from tkinter import*
 from tkinter import ttk
 from turtle import clear
 
-
 tela_entrar = Tk()
-tela_entrar.geometry('800x450+300+200')
+tela_entrar.geometry('800x450+300+100')
 tela_entrar.resizable(width=0, height=0)
+tela_entrar.title('BARBERSHOP')
 
 note = ttk.Notebook(tela_entrar)
 note.place(x=5, y=0, width=790, height=440)
@@ -17,13 +17,8 @@ note.add(tela1, text='tela de envio')
 tela2 = Frame(tela_entrar,bg='#D8E1FF', borderwidth= 2, relief='sunken')
 note.add(tela2, text='tela de relatório')
 
-#criação de arquivo txt que ira coletar os dados enviados do sistema e armazenar em um arquivo txt.
-def envio(): #parametro (a) irá enviar os dados do sistema para um arquivo txt e (arquivo) é a variável.
-    with open('salvando_dados.txt', 'a') as arquivo: #parametro (a) irá enviar os dados do sistema para um arquivo txt
-        arquivo.write(' Nome: {}\n barbeiro: {}\n Serviço: {}\n Valor Pago: {}\n Forma de Pagamento: {}\n'.format(nome_coleta,barbeiro_coleta,servico_coleta,valor_coleta,pag_coleta))
-        print('dados armazenados com sucesso.')
-
-def imprimir(): 
+#dados da tela1
+def armazenar(): 
     #função que ira coletar os dados gerais colocados no sistema e ira atribuir a outras variaveis que serão exibidas por meio de print 
     
     global nome_coleta,barbeiro_coleta,servico_coleta,valor_coleta,pag_coleta #global deixa a variável alocada em todo o cód
@@ -33,13 +28,16 @@ def imprimir():
     servico_coleta = cb_servicos.get()
     valor_coleta = float(caixa_valor.get())
     pag_coleta = cb_pagamento.get()
-    print(' CLIENTE: {} \n BARBEIRO: {}\n SERVIÇO: {}\n VALOR: {} \n FORMA DE PAGAMENTO: {}\n'.format(nome_coleta, barbeiro_coleta, servico_coleta, valor_coleta, pag_coleta ))
-    
+    with open('salvando_dados.txt', 'a') as arquivo:
+        arquivo.write(' CLIENTE: {} \n BARBEIRO: {}\n SERVIÇO: {}\n VALOR: {} \n FORMA DE PAGAMENTO: {}\n'.format(nome_coleta, barbeiro_coleta, servico_coleta, valor_coleta, pag_coleta ))
+        arquivo.close()
+        print('DADOS SALVOS')
+        
 def limpar(): #função que limpar as determinadas variáveis
     caixa_nome.delete(0,END)
     caixa_nome_barbeiro.delete(0,END)
     caixa_valor.delete(0,END)
-    
+
 lista_servicos =['BARBA', 'CORTE NA TESOURA', 'CORTE TESOURA E MÁQUINA']    #lista de serviços da barbearia
 lista_pagamento = ['AVISTA','CARTÃO', 'PIX']    #lista das possiveis formas de pagamento
 
@@ -86,17 +84,47 @@ cb_pagamento.set('SELECIONE')
 cb_pagamento.place(x=400, y=250)
 
 
-b1=Button(tela1, text='ARMAZENAR',command = envio)
+b1=Button(tela1, text='ARMAZENAR', command=armazenar)
 b2=Button(tela1, text='LIMPAR', command = limpar)
-b3=Button(tela1, text='DADOS DO DIA')
 
 b1.place(x=150, y=320, width = 130)
-b2.place(x=310, y=320, width = 100)
-b3.place(x=450, y=320, width = 130)
+b2.place(x=480, y=320, width = 100)
 
+#dados da tela2
 
+cod_cad = Label(tela2, text='CÓDIGO', bg='#D8E1FF')
+cod_cad.place(x=1, y=1)
+caixa_cod_cad = Entry(tela2, border=2)
+caixa_cod_cad.place(x=60, y=1, width = 50)
 
+nome_cad = Label(tela2, text='NOME', bg='#D8E1FF')
+nome_cad.place(x=150, y=1)
+caixa_nome_cad = Entry(tela2, border=2)
+caixa_nome_cad.place(x=200, y=1, width = 250)
 
+fone_cad = Label(tela2, text='TELEFONE',  bg='#D8E1FF')
+fone_cad.place(x=470, y=1)
+caixa_fone_cad = Entry(tela2, border=2)
+caixa_fone_cad.place(x=550, y=1, width = 150)
+
+end_cad = Label(tela2, text='ENDEREÇO', bg='#D8E1FF')
+end_cad.place(x=1, y=40)
+caixa_end_cad = Entry(tela2, border=2)
+caixa_end_cad.place(x=80, y=40, width = 150)
+
+email_cad = Label(tela2, text='E-MAIL', bg='#D8E1FF')
+email_cad.place(x=250, y=40 )
+caixa_email_cad = Entry(tela2, border=2)
+caixa_email_cad.place(x=310, y=40, width = 250)
+
+lista_sexo =['MASCULINO', 'FEMININO', 'OUTROS']
+
+lb_sexo = Label(tela2, text='SEXO',bg='#D8E1FF')
+lb_sexo.place(x=580, y=40)
+
+cb_sexo = ttk.Combobox(tela2, values=lista_sexo, state='readionly')
+cb_sexo.set('SELECIONE')
+cb_sexo.place(x=620, y=40)
 
 
 
